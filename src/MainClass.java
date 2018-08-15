8bd823a636642d3d98b8e5527033ab2bc52222d7
ivan-ristovic <ivo.ristovic@gmail.com>
2018-08-15 09:24:29 -0700

    [Misc] Playing around with API

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,8 +5,14 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.NoSuchElementException;
 
+import com.github.gumtreediff.actions.ActionGenerator;
+import com.github.gumtreediff.actions.model.Action;
 import com.github.gumtreediff.client.Run;
 import com.github.gumtreediff.gen.Generators;
+import com.github.gumtreediff.gen.jdt.cd.EntityType;
+import com.github.gumtreediff.matchers.MappingStore;
+import com.github.gumtreediff.matchers.Matcher;
+import com.github.gumtreediff.matchers.Matchers;
 import com.github.gumtreediff.tree.ITree;
 import com.github.gumtreediff.tree.Tree;
 import com.github.gumtreediff.tree.TreeContext;
@@ -21,50 +27,65 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		try {
-			TreeContext tctx1 = Generators.getInstance().getTree("tests/test1.cpp");
-			TreeContext tctx2 = Generators.getInstance().getTree("tests/test2.cpp");
-			ITree t1 = tctx1.getRoot();
-			ITree t2 = tctx2.getRoot();
+			ITree t1 = Generators.getInstance().getTree("tests/test1.cpp").getRoot();
+			ITree t2 = Generators.getInstance().getTree("tests/test2.cpp").getRoot();
 			
-			/*
-			System.out.println(t1.toTreeString());
-			System.out.println(t2.toStaticHashString());
+			// printMyWay(t1, true);
 			
-			fuck this
-			*/
+			Matcher m = Matchers.getInstance().getMatcher(t1, t2);
+			m.match();
 			
-			printMyWay(t1);
+			MappingStore st = m.getMappings();
+			
+			ActionGenerator g = new ActionGenerator(t1, t2, st);
+			List<Action> actions = g.generate();
+			
+			for (Action action : actions) {
+				System.out.print(action.getName());
+				System.out.print(" :");
+				printMyWay(action.getNode(), false);
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	
-	private static void printMyWay(ITree t)
+	private static void printMyWay(ITree tree, boolean recursive)
 	{
-		StringBuilder sb = new StringBuilder(new String(new char[t.getDepth()]).replace("\0", "    "));
-		
-		sb.append("[");
-		if (t.hasLabel())
-			sb.append(t.getLabel());
-		else
-			sb.append("no label");
-		sb.append("] pos: ");
-		sb.append(t.getPos());
+		for (ITree t : tree.preOrder()) {
+			StringBuilder sb = new StringBuilder(new String(new char[t.getDepth()]).replace("\0", "    "));
+			
+			sb.append("[");
+			if (t.hasLabel())
+				sb.append(t.getLabel());
+			else
+				sb.append("?");
+			sb.append("] pos: ");
+			sb.append(t.getPos());
+			sb.append(", endpos: ");
+			sb.append(t.getEndPos());
+			sb.append(", size: ");
+			sb.append(t.getSize());
+			sb.append(", type: ");
+			sb.append(t.getType());
+			
+			sb.append(" | metadata: [");
+			for (Iterator<Entry<String, Object>> it = t.getMetadata(); it.hasNext(); ) {
+				Entry<String, Object> entry = it.next();
+				sb.append(entry.getKey());
+				sb.append(":");
+				sb.append(entry.getValue().toString());
+				sb.append(" ");
+			}
+			sb.append("]");
 
-		sb.append(" metadata: [ ");
-		for (Iterator<Entry<String, Object>> it = t.getMetadata(); it.hasNext(); ) {
-			Entry<String, Object> entry = it.next();
-			sb.append(entry.getKey());
-			sb.append(":");
-			sb.append(entry.getValue().toString());
-			sb.append(" ");
+			
+			System.out.println(sb.toString());
+			
+			if (!recursive)
+				return;
 		}
-		sb.append("]");
-		
-		System.out.println(sb.toString());
-		
-		for (ITree child : t.getChildren()) 
-			printMyWay(child);
 	}
 }
