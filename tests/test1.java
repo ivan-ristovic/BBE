444a217f2a75c47613ce97ba37c2320c177cdee8
filozof50 <strahinjastanojevic@rocketmail.com>
2018-08-20 21:58:28 +0200

    Update test1.java

--- tests/test1.java
+++ tests/test1.java
@@ -5,9 +5,34 @@ public class A
     private static void foo() 
     {
         int i = 9;
-        int j, k;
+        int jot, k, r = 0;
         ArrayList<Integer> al = new ArrayList<Integer>();
         
+        jot = 345;
+        
+        i += 5;
+        
+        jot -= 5;
+
+        k = 19;
+        
+        k++;
+        
+        ++i;
+        
+        --k;
+        
+        r += i;
+        
+        int x = i + k;
+
+        int m = 4 * 3;
+        
+        if (m < 3)
+        	k++;
+        
         foo();
+        
+        return x;
     }
 }
